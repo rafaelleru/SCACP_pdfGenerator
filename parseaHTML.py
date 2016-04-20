365bd6111a934315c88807276f8977801de3b862
rafa <rafaelleru95103@gmail.com>
2016-04-20 12:27:24 +0200

    creada clase pregunta añadidas funciones

---
+++ parseaHTML.py
@@ -0,0 +1,9 @@
+class Pregunta(object):
+    """Pregunta del cuestionario"""
+    enunciado
+    respuestas[]
+    correcta
+
+    def __init__(self):
+        super(Pregunta, self).__init__()
+        self.arg = arg
