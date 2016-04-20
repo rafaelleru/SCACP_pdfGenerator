b248f45a0a5ac5fff1519861be7ae843b4c60e24
rafa <rafaelleru95103@gmail.com>
2016-04-20 15:56:05 +0200

    sergio sirolo

--- parseaHTML.py
+++ parseaHTML.py
@@ -13,4 +13,23 @@ class Pregunta(object):
 def NuevaPregunta():
 
 def leeHTML(archivo):
-    archivo.readline()
+    a_descartar = archivo.readline()
+    while a_descartar is not '<div class="cajaGift">'
+        a_descartar = archivo.readline()
+
+    linea = archivo.readline()
+    #linea.repla
+    if linea.startswith('<div class="respuestasGift">')
+        #leer la linea entera || Ahora lo hare
+        for j in range(0, 5):
+            cadena = cadena + linea[j]
+        if cadena is "&nbsp":
+            #na
+            break;
+        elif cadena is "<stro":
+            caracter = ""
+            while caracter is not ">":
+                a_descartar = linea.getchar()
+            enunciado = ""
+            while caracter is not "<":
+                enunciado = enunciado + caracter
